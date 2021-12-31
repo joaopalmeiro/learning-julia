367924d7dbc5c24d73f607c0df11a4cbb62ab643
João Palmeiro <joaommpalmeiro@gmail.com>
2021-12-31 18:08:46 +0000

    Create nucleotide-count.jl

---
+++ exercism/nucleotide-count.jl
@@ -0,0 +1,17 @@
+function count_nucleotides(strand)
+    default_count = 0
+    nucleotides = "ACGT"
+
+    res = Dict(n => default_count for n in nucleotides)
+
+    for n in strand
+        # https://docs.julialang.org/en/v1/manual/strings/#Common-Operations
+        if occursin(n, nucleotides)
+            res[n] = res[n] + 1
+        else
+            throw(DomainError("Invalid strand"))
+        end
+    end
+
+    res
+end
